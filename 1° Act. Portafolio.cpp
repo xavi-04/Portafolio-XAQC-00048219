33254679b6e1665d9e4302d37826b17b14937656
xavi-04 <chavidelcid@gmail.com>
2020-01-21 10:30:34 -0600

    Add: Portafile

---
+++ 1° Act. Portafolio.cpp
@@ -0,0 +1,21 @@
+//Calcular la mediana
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int size = 0;
+
+    cout << "Digite el tamaño: ";
+    cin >> size;
+    
+    for (int i = 0; i < size; i++)
+    {
+       cout << "Escriba los números: " << endl; 
+       cin >> size;
+    }
+    
+    return 0;
+}
+
